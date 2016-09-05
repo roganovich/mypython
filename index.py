11714050ee3f9f93b1eeb8b85dd307d3eeb8d184
Soft Sandbay <soft@sandbay.ru>
2016-09-05 17:08:25 +0300

    start my best parser object dahua

--- index.py
+++ index.py
@@ -2,12 +2,19 @@
 import sys
 #Добавляем путь к собственным модулям
 sys.path.append('modules')
+
+
+
 import baseclass #несмотря на предупреждение модуль подключился
 import dahua #несмотря на предупреждение модуль подключился
 
 baseclass.testFunc('check add new module') #вызываем метод из модуля
 
-dahuaparser = dahua.dahua()
-dahuaparser.setAttr('ulr','dahua.com')
-url = dahuaparser.getAttr('ulr')
-print(url)
+dp= dahua.dahua()
+dp.setAttr('url','http://dahua-russia.ru')
+dp.startParser()
+soup = dp.getPageObj()
+
+print(soup.prettify())
+
+
--- modules/dahua.py
+++ modules/dahua.py
@@ -1,10 +1,20 @@
 # coding:utf8
+import urllib.request
+from bs4 import BeautifulSoup
 
 class dahua:
     attr = {}
+    pageObj = None
 
     def setAttr(self,name,value):
         self.attr[name]=value
 
     def getAttr(self, name):
         return self.attr[name]
+
+    def startParser(self):
+        page =  urllib.request.urlopen(self.attr['url']).read()
+        self.pageObj = BeautifulSoup(page)
+
+    def getPageObj(self):
+        return self.pageObj
